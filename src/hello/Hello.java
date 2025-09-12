37616800aa0bbb455e41870303a38534668ef5ca
Sushanth <146927710+ALLURKARSUSHANTH@users.noreply.github.com>
2025-09-12 14:26:20 +0530

    Fix typo in web hook message

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("I am Allurkar Sushanth");
-    System.out.println("Checking Web hook");
+    System.out.println("Checking Web hooks");
   }
 }
